e8852303956b3b18820c13faaf6b12ff932559e6
Jackson <cyber.jso@gmail.colm>
2015-04-20 17:48:02 -0300

    Consuming files from s3

---
+++ camel/s3-integration/build.sbt
@@ -0,0 +1,13 @@
+name := """s3-integration"""
+
+version := "1.0"
+
+scalaVersion := "2.11.5"
+
+libraryDependencies ++= Seq(
+  "org.apache.camel" % "camel-core" % "2.15.0",
+  "org.apache.camel" % "camel-aws" % "2.15.0",
+  "commons-io" % "commons-io" % "2.4"
+)
+
+mainClass := Some("com.example.Main")
---
+++ camel/s3-integration/src/main/scala/com/example/S3Integration.scala
@@ -0,0 +1,76 @@
+package com.example
+
+import org.apache.camel.component.aws.s3.S3Constants
+import org.apache.camel.{Message, Exchange, Processor}
+import org.apache.camel.builder.RouteBuilder
+
+
+class MyRouteBuilder extends RouteBuilder {
+
+  override def configure(): Unit ={
+    from("aws-s3://json-in?amazonS3Client=#client&maxMessagesPerPoll=15&delay=3000&region=sa-east-1").
+      to("bean:fileProcessor").
+      choice().
+        when(header("status").isEqualTo("ok")).
+          to("aws-s3://json-out?amazonS3Client=#client&region=sa-east-1").
+        otherwise().
+          to("aws-s3://json-error?amazonS3Client=#client&region=sa-east-1")
+  }
+
+}
+
+class FileProcessor extends Processor {
+  import java.util.logging.Logger
+
+  val logger = Logger.getLogger(classOf[FileProcessor].getName)
+
+  override def process(msg: Exchange): Unit ={
+    val content = msg.getIn.getBody(classOf[String])
+    // Do Whatever you need with the content
+    logger.info(content)
+    Messenger.send(message = msg, status = Some("ok"))
+  }
+
+}
+
+object Messenger {
+  import org.apache.commons.io.IOUtils
+
+  def send(message: Exchange, status: Option[String]): Unit ={
+    message.getOut.setHeader("status", status.getOrElse("error"))
+    message.getOut.setHeader(S3Constants.KEY,  message.getIn.getHeader(S3Constants.KEY).asInstanceOf[String])
+    message.getOut.setHeader(S3Constants.BUCKET_NAME, message.getIn.getHeader(S3Constants.BUCKET_NAME))
+    message.getOut.setBody(IOUtils.toInputStream(message.getIn.getBody(classOf[String])))
+  }
+
+}
+
+object CamelContext {
+  import com.amazonaws.auth.BasicAWSCredentials
+  import com.amazonaws.services.s3.AmazonS3Client
+  import org.apache.camel.main.Main
+
+  val accessKey = ""
+  val secretKey = ""
+
+  def start(): Unit ={
+
+    def  validateConfig(): Unit= {
+      if (secretKey.isEmpty) throw new IllegalStateException("AWS secret key  is not defined")
+      if (accessKey.isEmpty) throw new IllegalStateException("AWS access key  is not defined")
+    }
+
+    validateConfig()
+    val camel  = new Main
+    camel.bind("client", new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey)))
+    camel.bind("fileProcessor", new FileProcessor)
+    camel.addRouteBuilder(new MyRouteBuilder)
+    camel.enableHangupSupport()
+    camel.run()
+  }
+
+}
+
+object Main extends App {
+  CamelContext.start()
+}
