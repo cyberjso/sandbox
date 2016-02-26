26b7dd2e74bfb36caf8da8324d502ef917b99dff
Jackson <cyber.jso@gmail.colm>
2016-02-25 22:52:27 -0300

    java8 futures

---
+++ non-blocking-io/java8-completable-futtures/src/main/java/io/sandbox/nonblockingio/Main.java
@@ -0,0 +1,9 @@
+package io.sandbox.nonblockingio;
+
+public class Main {
+
+    public static  void main(String[] args) {
+        new Reader().read();
+    }
+
+}
---
+++ non-blocking-io/java8-completable-futtures/src/main/java/io/sandbox/nonblockingio/Reader.java
@@ -0,0 +1,35 @@
+package io.sandbox.nonblockingio;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Reader {
+    private List<String> files = Arrays.asList("file-3.txt", "file-1.txt", "file-2.txt");
+    private static final Logger logger = LogManager.getLogger(Reader.class);
+
+    public void read() {
+        CompletionService service = new ExecutorCompletionService<List<String>>(Executors.newFixedThreadPool(3));
+        files.stream().forEach(file -> service.submit(() ->
+                FileUtils.readLines(new File(this.getClass().getClassLoader().getResource(file).getFile()))));
+        files.stream().forEach(file -> {
+            try  {
+                Future<List<String>> fileContent = service.take();
+                logger.info(fileContent.get().size());
+            } catch (Exception e ) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+
+
+}
