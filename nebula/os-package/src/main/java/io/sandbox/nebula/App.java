700fe91785a6cd9373589ed32d056a930fd8b5b6
Jackson <cyber.jso@gmail.colm>
2016-04-14 22:45:50 -0300

    Nebula gradle plugin packging
    First commit

---
+++ nebula/os-package/src/main/java/io/sandbox/nebula/App.java
@@ -0,0 +1,21 @@
+package io.sandbox.nebula;
+
+
+import org.apache.log4j.Logger;
+
+public class App {
+    private static Logger logger =  Logger.getLogger(App.class);
+
+    public static void main(String args[]) {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                logger.info("*** ping ***");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
