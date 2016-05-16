d52b002f7cdb2b63ef296ea3eb3ff3ef5d4cbbf2
Jackson <cyber.jso@gmail.colm>
2016-05-16 17:37:08 -0300

    Word Counter using RXJava

---
+++ ReactiveX/rx-reding-from-file/src/main/java/io/sandbox/rx/FileReader.java
@@ -0,0 +1,39 @@
+package io.sandbox.rx;
+
+import javafx.util.Pair;
+import rx.Observable;
+import rx.Subscriber;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class FileReader {
+
+    public void read(String fileName) {
+
+        Observable.create(new Observable.OnSubscribe<String>() {
+
+            public void call(Subscriber<? super String> subscriber) {
+                try {
+                    Files.lines(Paths.get(FileReader.class.getResource("/" + fileName).toURI()), Charset.defaultCharset())
+                            .forEach(line -> Arrays.stream(line.split(" "))
+                                    .forEach( word -> subscriber.onNext(word)));
+                    subscriber.onCompleted();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        })
+        .filter(word -> !word.trim().isEmpty())
+        .groupBy(word -> word.trim())
+        .flatMap(words -> words.count().map(counter -> new Pair(words.getKey(), counter)))
+        .toList()
+        .subscribe(pairs ->
+                pairs.forEach(
+                        item -> System.out.println("key: " + item.getKey() + " value: " + item.getValue())));
+    }
+
+}
---
+++ ReactiveX/rx-reding-from-file/src/main/java/io/sandbox/rx/Main.java
@@ -0,0 +1,9 @@
+package io.sandbox.rx;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new FileReader().read("myFile.txt");
+    }
+
+}
