b76ee33d1a854f2122d00e23800a3a5e0e4f2c3c
Jackson <cyber.jso@gmail.colm>
2016-07-28 11:07:23 -0300

    Applying filter to ignore job tracker

--- python/jenkins-api/job-tracker/__main__.py
+++ python/jenkins-api/job-tracker/__main__.py
@@ -13,7 +13,8 @@ def get_running_jobs():
     running_jobs = []
     for j in server.get_jobs():
         job_instance = server.get_job(j[0])
-        if job_instance.is_running():
+        print job_instance
+        if job_instance.is_running() and job_instance.name != 'job-tracker':
             running_jobs.append(job_instance.name)
 
     return running_jobs        
