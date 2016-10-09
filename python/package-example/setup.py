a1b5195064f2c539fcebafbe97dff9e2304a3cf4
Jackson <cyber.jso@gmail.colm>
2016-10-09 14:52:15 -0300

    Package examples

---
+++ python/package-example/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='package-example',
+      version='0.1',
+      description='This is just an example about how to make python packages',
+      url='http://github.com/storborg/funniest',
+      author='Jackson Oliveira',
+      author_email='jackson.oliveira.rs@gmail.com',
+      license='MIT',
+      packages=['package-example'],
+      zip_safe=False)
