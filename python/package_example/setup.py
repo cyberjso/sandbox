8a79be27a2dfd6e2641630615d7bb2519ac74044
Jackson <cyber.jso@gmail.colm>
2016-10-09 17:16:29 -0300

    Name refactoring

--- python/package_example/setup.py
+++ python/package_example/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='package-example',
+setup(name='package_example',
       version='0.1',
       description='This is just an example about how to make python packages',
       url='https://github.com/cyberjso/sandbox/tree/master/python/package-example',
       author='Jackson Oliveira',
       author_email='jackson.oliveira.rs@gmail.com',
       license='MIT',
-      packages=['package-example'],
+      packages=['package_example'],
       zip_safe=False)
