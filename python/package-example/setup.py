eed5f9eaded52edf7417a304639e883dd474079f
Jackson <cyber.jso@gmail.colm>
2016-10-09 17:01:12 -0300

    Git repo update

--- python/package-example/setup.py
+++ python/package-example/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='package-example',
       version='0.1',
       description='This is just an example about how to make python packages',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/cyberjso/sandbox/tree/master/python/package-example',
       author='Jackson Oliveira',
       author_email='jackson.oliveira.rs@gmail.com',
       license='MIT',
