efccfc8d9c793241e09c1334336cfc26e509549a
Jackson <cyber.jso@gmail.colm>
2017-02-05 23:00:07 -0200

    Working code reading from wiki

---
+++ stream/hello-flink/src/main/java/io/sandbox/stream/flink/Main.java
@@ -0,0 +1,8 @@
+package io.sandbox.stream.flink;
+
+public class Main {
+
+    public static  void main(String[] args) {
+        new WikiReader().read();
+    }
+}
---
+++ stream/hello-flink/src/main/java/io/sandbox/stream/flink/WikiReader.java
@@ -0,0 +1,35 @@
+package io.sandbox.stream.flink;
+
+import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+
+public class WikiReader {
+
+    public void read() {
+        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
+        KeyedStream<WikipediaEditEvent, String>  keyedEdits =  edits.keyBy((WikipediaEditEvent event) -> event.getUser());
+        DataStream<Tuple2<String, Long>> result  = keyedEdits
+                .timeWindow(Time.seconds(5))
+                .fold(new Tuple2("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
+                    public Tuple2<String, Long> fold(Tuple2<String, Long> accumulator, WikipediaEditEvent value) throws Exception {
+                        accumulator.f0 = value.getUser();
+                        accumulator.f1 += value.getByteDiff();
+                        return accumulator;
+                    }
+                });
+        result.print();
+        try {
+            see.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
